3d36c27c7e2baebb47cd65f2a692c6b574f98404
Edson Martins <edsonmartins2005@gmail.com>
2014-04-04 11:31:06 -0300

    Adicionado métodos SET que foram removidos. Estes são necessários para criar objeto por reflection.

--- src/br/com/anteros/persistence/sql/datasource/JDBCDataSource.java
+++ src/br/com/anteros/persistence/sql/datasource/JDBCDataSource.java
@@ -95,4 +95,36 @@ public class JDBCDataSource implements DataSource {
 		return null;
 	}
 
+	public String getDriverClassName() {
+		return driverClassName;
+	}
+
+	public void setDriverClassName(String driverClassName) {
+		this.driverClassName = driverClassName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
 }
